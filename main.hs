7402e66c4ca8d2481002b4670ed2efddbd0cae05
Sam Kellar <Sam Kellar>
2016-12-08 14:59:10 -0500

    Fixed compilation warnings

--- main.hs
+++ main.hs
@@ -2,30 +2,33 @@
 data Tree = Branch { leftChild :: Tree, value :: Char, rightChild :: Tree } | Leaf { value :: Char } deriving (Show)
 
 listToTree :: [Char] -> (Tree, [Char])
+listToTree [] = (Leaf { value = '?' }, [])
 listToTree (c:cs)
-	| c == '*' = (Branch { leftChild = fst leftSubTree, value = c, rightChild = fst rightSubTree }, snd rightSubTree)
-	| otherwise = (Leaf { value = c }, cs)
-	where	
-		leftSubTree = listToTree cs
-		rightSubTree = listToTree $ snd leftSubTree
+    | c == '*' = (Branch { leftChild = fst leftSubTree, value = c, rightChild = fst rightSubTree }, snd rightSubTree)
+    | otherwise = (Leaf { value = c }, cs)
+    where    
+        leftSubTree = listToTree cs
+        rightSubTree = listToTree $ snd leftSubTree
 
 preorderTraverse :: Tree -> [Char] -> (Char, [Char])
 preorderTraverse (Leaf v) [] = (v, [])
 preorderTraverse (Leaf v) (cs) = (v, cs)
-preorderTraverse (Branch l v r) (c:cs)
-	| c == '0' = preorderTraverse l cs
-	| otherwise = preorderTraverse r cs
+preorderTraverse (Branch _ _ _) [] = ('?', [])
+preorderTraverse (Branch l _ r) (c:cs)
+    | c == '0' = preorderTraverse l cs
+    | otherwise = preorderTraverse r cs
 
 decode :: Tree -> [Char] -> [Char] -> [Char]
-decode t (cs) [] = reverse cs
+decode _ (cs) [] = reverse cs
 decode t (cs) (cs') = decode t (decoded:cs) (snd traversed)
-	where
-		traversed = preorderTraverse t (cs')
-		decoded = fst traversed
+    where
+        traversed = preorderTraverse t (cs')
+        decoded = fst traversed
 
-split :: [[Char]] -> [[Char]]		
+split :: [[Char]] -> [[Char]]
+split [] = [[]]    
 split (s:ss) = [ decode mTree "" s' | s' <- ss]
-	where mTree = fst $ listToTree s
+    where mTree = fst $ listToTree s
 
 main :: IO ()
 main = interact (unlines . split . lines)
